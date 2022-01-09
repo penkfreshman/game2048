06687f1c35d1debc174d98f7e9d45829a0290f32
penkfreshman <1561435601@qq.com>
2022-01-10 00:04:27 +0800

    UI make1

--- app/src/main/java/com/example/game2048/Card.java
+++ app/src/main/java/com/example/game2048/Card.java
@@ -1,90 +1,110 @@
+
+
 package com.example.game2048;
-
-
 import android.content.Context;
-import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 public class Card extends FrameLayout {
-    FrameLayout.LayoutParams lp;
-    private View backgroud;
-    private  TextView tv;
-    private  int num=0;
+    private TextView label;
+    private View background;
 
-    public Card(@NonNull Context context) {
+    public Card(Context context) {
         super(context);
-        backgroud=  new View(getContext());
-        addView(backgroud,lp);
 
-        tv =new TextView(getContext());
-        tv.setTextSize(28);
-        tv.setGravity(Gravity.CENTER);
+        LayoutParams lp = null;
 
-        lp=new LayoutParams(-1,-1);
-        lp.setMargins(10,10,0,0);
-        addView(tv,lp);
+        background = new View(getContext());
+        lp = new LayoutParams(-1, -1);
+        lp.setMargins(20, 20, 0, 0);
+        background.setBackgroundColor(0x33ffffff);
+        addView(background, lp);
+
+        label = new TextView(getContext());
+        label.setTextSize(28);
+        label.setGravity(Gravity.CENTER);
+
+        lp = new LayoutParams(-1, -1);
+        lp.setMargins(20, 20, 0, 0);
+        addView(label, lp);
+
+        setNum(0);
     }
 
-    public   int getNum(){
+
+    private int num = 0;
+
+    public int getNum() {
         return num;
     }
 
-    public  void setNum(int Num){
+    public void setNum(int num) {
+        this.num = num;
+
         if (num<=0) {
-            tv.setText("");
+            label.setText("");
         }else{
-            tv.setText(num+"");
+            label.setText(num+"");
         }
 
         switch (num) {
             case 0:
-                tv.setBackgroundColor(0x00000000);
+                label.setBackgroundColor(0x00000000);
                 break;
             case 2:
-                tv.setBackgroundColor(0xffeee4da);
+                label.setBackgroundColor(0xffeee4da);
                 break;
             case 4:
-                tv.setBackgroundColor(0xffede0c8);
+                label.setBackgroundColor(0xffede0c8);
                 break;
             case 8:
-                tv.setBackgroundColor(0xfff2b179);
+                label.setBackgroundColor(0xfff2b179);
                 break;
             case 16:
-                tv.setBackgroundColor(0xfff59563);
+                label.setBackgroundColor(0xfff59563);
                 break;
             case 32:
-                tv.setBackgroundColor(0xfff67c5f);
+                label.setBackgroundColor(0xfff67c5f);
                 break;
             case 64:
-                tv.setBackgroundColor(0xfff65e3b);
+                label.setBackgroundColor(0xfff65e3b);
                 break;
             case 128:
-                tv.setBackgroundColor(0xffedcf72);
+                label.setBackgroundColor(0xffedcf72);
                 break;
             case 256:
-                tv.setBackgroundColor(0xffedcc61);
+                label.setBackgroundColor(0xffedcc61);
                 break;
             case 512:
-                tv.setBackgroundColor(0xffedc850);
+                label.setBackgroundColor(0xffedc850);
                 break;
             case 1024:
-                tv.setBackgroundColor(0xffedc53f);
+                label.setBackgroundColor(0xffedc53f);
                 break;
             case 2048:
-                tv.setBackgroundColor(0xffedc22e);
+                label.setBackgroundColor(0xffedc22e);
                 break;
             default:
-                tv.setBackgroundColor(0xff3c3a32);
+                label.setBackgroundColor(0xff3c3a32);
                 break;
         }
-
     }
+
+    public boolean equals(Card o) {
+        return getNum()==o.getNum();
+    }
+
+    protected Card clone(){
+        Card c= new Card(getContext());
+        c.setNum(getNum());
+        return c;
+    }
+
+    public TextView getLabel() {
+        return label;
+    }
+
 
 }
--- app/src/main/java/com/example/game2048/gameView.java
+++ app/src/main/java/com/example/game2048/gameView.java
@@ -1,48 +1,137 @@
 package com.example.game2048;
 
-import android.content.Context;
-import android.util.AttributeSet;
-import android.widget.GridView;
+import java.util.ArrayList;
+import java.util.List;
 
-public class gameView extends GridView {
-    public int NUM=4;
-    public static int CARD_WIDTH=0;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Point;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+
+public class gameView extends GridLayout {
+    private static int NUM=4;
+    private static int CARD_WIDTH=0;
+
     public gameView(Context context) {
         super(context);
-        initLayout();
+
+        initGameView();
     }
 
     public gameView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        initLayout();
+
+        initGameView();
     }
 
-    public gameView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        initLayout();
-    }
-    public void initLayout(){
-        setNumColumns(NUM);
-        Card c=new Card(getContext());
-        setBackgroundColor(getResources().getColor(R.color.theme2048));
+    private void initGameView(){
+       setColumnCount(NUM);
+       setBackgroundColor(getResources().getColor(R.color.theme2048));
+       addCards(GetCardWidth(),GetCardWidth());
 
+        setOnTouchListener(new OnTouchListener() {
+            private float startX,startY,offsetX,offsetY;
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()){
+                    case MotionEvent.ACTION_DOWN:{
+                        startX=motionEvent.getX();
+                        startY=motionEvent.getY();
+
+                        break;}
+                    case MotionEvent.ACTION_UP:{
+                        offsetX=motionEvent.getX()-startX;
+                        offsetY=motionEvent.getY()-startY;
+                        if(Math.abs(offsetX)>Math.abs(offsetY)){
+                            if(offsetX<-5) SwipeLeft();
+                            else if (offsetX>5) SwipeRight();
+                        }else{
+                            if(offsetY<-5) Swipeup();
+                            else if (offsetY>5) Swipedwon();
+                        }
+
+                        break;
+                    }
+                }
+                return true;
+            }
+        });
+    }
+
+    private void Swipedwon() {
+        Log.d("gameview","down");
+    }
+
+    private void Swipeup() {
+        Log.d("gameview","up");
+    }
+
+    private void SwipeRight() {
+        Log.d("gameview","right");
+    }
+
+    private void SwipeLeft() {
+        Log.d("gameview","left");
+        
     }
 
     @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        CARD_WIDTH=(Math.min(w,h)-10)/NUM;
-        addCard(CARD_WIDTH,CARD_WIDTH);
+    protected void onMeasure(int widthSpec, int heightSpec) {
+        super.onMeasure(widthSpec, heightSpec);
+
+
     }
 
-    public  void addCard(int widht ,int height ){
-            for(int x=0;x<NUM;x++){
-                for(int y=0;y<NUM ;y++){
-                    Card c=new Card(getContext());
-                    c.setNum(2);
-                    addView(c,widht,height);
-                }
+    private int GetCardWidth()
+    {
 
+        //屏幕信息的对象
+        DisplayMetrics displayMetrics;
+        displayMetrics = getResources().getDisplayMetrics();
+
+        //获取屏幕信息
+        int cardWidth;
+        cardWidth = displayMetrics.widthPixels;
+
+        cardWidth=cardWidth-40;
+        Log.d("height1",cardWidth+" ");
+        GridLayout gridLayout=findViewById(R.id.gameView);
+        GridLayout.LayoutParams params= new LayoutParams();
+        params.height=cardWidth;
+        params.width=cardWidth;
+        gridLayout.setLayoutParams(params);
+
+        Log.d("width_layout",cardWidth+"");
+        //一行有四个卡片，每个卡片占屏幕的四分之一
+        return  (cardWidth/NUM)-20;
+
+    }
+
+
+
+    private void addCards(int cardWidth,int cardHeight){
+
+        Log.d("width",cardWidth+"");
+        Card c;
+        for (int y = 0; y < NUM; y++) {
+            for (int x = 0; x < NUM; x++) {
+                c = new Card(getContext());
+                c.setNum(2);
+                addView(c,cardWidth,cardHeight);
+
+                cardsMap[x][y] = c;
             }
+        }
     }
+
+
+    private Card[][] cardsMap = new Card[NUM][NUM];
+    private List<Point> emptyPoints = new ArrayList<Point>();
 }
